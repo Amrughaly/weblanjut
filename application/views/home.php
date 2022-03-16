d84ac3f50ade4cc4010b9a33aa9fcb24e26655a6
Amrughaly <amrughaly@gmail.com>
2022-03-16 12:41:40 +0700

    pertama commit

---
+++ application/views/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Testing</title>
+    </head>
+
+    <body>
+        <h1>Hello World. Kukira Kau, Rumah?</h1>
+    </body>
+</html>
---
+++ application/views/penerima.php
@@ -0,0 +1,3 @@
+<<?php 
+	echo "data yang diterima adalah ".$test;
+ ?>
